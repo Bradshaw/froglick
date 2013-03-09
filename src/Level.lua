8825a59b5e20df9559582eb009808f3f9e7833c1
wilbefast <wilbefast@gmail.com>
2013-03-09 18:00:51 +0100

    added Level.lua

---
+++ src/Level.lua
@@ -0,0 +1,69 @@
+--[[
+Copyright (C) 2013 William Dyce, Kevin Bradshaw and Hannes Delbeke
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/.
+--]]
+
+
+--[[----------------------------------------------------------------------------
+METATABLE (PROTOTYPE)
+--]]----------------------------------------------------------------------------
+
+local Level_mt = {}
+
+
+--[[----------------------------------------------------------------------------
+CLASS METHODS
+--]]----------------------------------------------------------------------------
+
+function Level_mt(self, dt)
+  --! override me
+end
+
+  
+--[[----------------------------------------------------------------------------
+CLASS
+--]]----------------------------------------------------------------------------
+
+local Level = {}
+
+
+--[[----------------------------------------------------------------------------
+CLASS (STATIC) FUNCTIONS
+--]]----------------------------------------------------------------------------
+
+--! this is a 'singleton': we can only have one in memory at a time
+function Level.get()
+  
+  local self
+  
+  if Level.instance then
+    self = Level.instance
+  else
+    self = {}
+    setmetatable(self, {__index = Level_mt })
+  end
+  
+  return self
+end
+
+function Level.clear()
+  Level.instance 
+
+
+--[[----------------------------------------------------------------------------
+EXPORT THE CLASS
+--]]----------------------------------------------------------------------------
+
+return Level, Level_mt
