0e68676edf42c7361dab3f87ddf76e936dbeff2f
Kevin "Gaeel" Bradshaw-Rodriguez <k@xi.gd>
2013-03-10 18:56:54 +0100

    Using Hannes' initial tile art BS level gen

--- src/LevelGen/init.lua
+++ src/LevelGen/init.lua
@@ -8,7 +8,7 @@ function LevelGen.new(n_cols, n_rows)
 	local lev = TileGrid.new(n_cols, n_rows)
 	for i,v in ipairs(lev.tiles) do
 		for j,u in ipairs(v) do
-			lev:gridToTile(vector(i,j)).wall=math.random(0,5)
+			lev:gridToTile(vector(i,j)).wall=math.max(0,math.min(5,math.floor((math.sin(i)+1)*1+(math.cos(j)+1)*1*2)))
 		end
 	end
 	return lev
