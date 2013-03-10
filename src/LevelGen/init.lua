12425aacdbb02ab520b93ad726031c2994931630
wilbefast <wilbefast@gmail.com>
2013-03-10 18:04:55 +0100

    fixed up the failed merge (my bad)

--- src/LevelGen/init.lua
+++ src/LevelGen/init.lua
@@ -8,7 +8,8 @@ function LevelGen.new(n_cols, n_rows)
 	local lev = TileGrid.new(n_cols, n_rows)
 	for row, v in ipairs(lev.tiles) do
 		for col, u in ipairs(v) do
-			lev:gridToTile(row, col).wall=math.max(0,math.min(5,math.floor((math.sin(i)+1)*1+(math.cos(j)+1)*1*2)))
+			lev:gridToTile(row, col).wall = math.max(0, 
+          math.min(5,math.floor((math.sin(row)+1)*1+(math.cos(col)+1)*1*2)))
 		end
 	end
 	return lev
