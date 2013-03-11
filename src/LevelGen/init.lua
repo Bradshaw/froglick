212511035422d7e0f45824875b36bdfa51c47172
Kevin "Gaeel" Bradshaw-Rodriguez <k@xi.gd>
2013-03-11 01:00:44 +0100

    Selectable level generator needs implementing yo...

--- src/LevelGen/init.lua
+++ src/LevelGen/init.lua
@@ -4,15 +4,19 @@ vector = require("vector")
 local LevelGen = {}
 
 
+LevelGen.generator = {}
+
+function LevelGen.generator.DEFAULT(lev)
+
+end
+
+
+
 function LevelGen.new(n_cols, n_rows)
 	local lev = TileGrid.new(n_cols, n_rows)
-	for row, v in ipairs(lev.tiles) do
-		for col, u in ipairs(v) do
-			lev:gridToTile(row, col).wall = math.max(0, 
-          math.min(5,math.floor((math.sin(row)+1)*1+(math.cos(col)+1)*1*2)))
-		end
-	end
+	LevelGen.generator.DEFAULT(lev)
 	return lev
 end
+
 
 return LevelGen
