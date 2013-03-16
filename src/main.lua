7c2285b79e1e2b455c0696172bcab66caf543551
Kevin "Gaeel" Bradshaw-Rodriguez <k@xi.gd>
2013-03-16 16:44:47 +0100

    NEW FOANT

--- src/main.lua
+++ src/main.lua
@@ -1,4 +1,26 @@
 function love.load(arg)
+	fontim = love.graphics.newImage("images/font.png")
+    fontim:setFilter("nearest","nearest")
+    font = love.graphics.newImageFont("images/myfont.png",
+    " abcdefghijklmnopqrstuvwxyz" ..
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ0" ..
+    "123456789.,!?-+/():;%_`'*__[]\"" ..
+    "<>&#=$")
+	--]]
+	--font = love.graphics.newFont("images/DisposableDroidBB.ttf",12)
+	translote = love.graphics.translate
+	love.graphics.translate = function(x,y)
+		translote(math.floor(x),math.floor(y))
+	end
+	pront = love.graphics.print
+	love.graphics.print = function(txt,x,y,...)
+		pront(txt,math.floor(x),math.floor(y),...)
+	end
+	drow = love.graphics.draw
+	love.graphics.print = function(txt,x,y,...)
+		pront(txt,math.floor(x),math.floor(y),...)
+	end
+    love.graphics.setFont(font)
 	local modes = love.graphics.getModes()
 	table.sort(modes, function(a, b) return a.width*a.height < b.width*b.height end)
 	local m = modes[#modes]
