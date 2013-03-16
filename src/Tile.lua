9fe0bced0f1c80e12b7f8fdd2bff6daf42c13fa0
Kevin "Gaeel" Bradshaw-Rodriguez <k@xi.gd>
2013-03-16 22:04:12 +0100

    Changed quads to reflect new image sizes

--- src/Tile.lua
+++ src/Tile.lua
@@ -82,14 +82,14 @@ table.insert(Tile.FULLQUADS,
 Tile.DECORATIONIMAGE = love.graphics.newImage("images/enviroment.PNG")
 Tile.DECOQUADS = {}
 Tile.DECOQUADS.HIGHLIGHTS = {
-  love.graphics.newQuad(0,0,64,64,256,256),
-  love.graphics.newQuad(64,0,64,64,256,256)
+  love.graphics.newQuad(0,0,64,64,256,160),
+  love.graphics.newQuad(64,0,64,64,256,160)
 }
 Tile.DECOQUADS.GRASS = {
-  love.graphics.newQuad(0,96,32,32,256,256),
-  love.graphics.newQuad(32,96,32,32,256,256),
-  love.graphics.newQuad(64,96,32,32,256,256),
-  love.graphics.newQuad(94,96,32,32,256,256)
+  love.graphics.newQuad(0,96,32,32,256,160),
+  love.graphics.newQuad(32,96,32,32,256,160),
+  love.graphics.newQuad(64,96,32,32,256,160),
+  love.graphics.newQuad(94,96,32,32,256,160)
 }
 
 
